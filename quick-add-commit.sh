37ac0ecacd3f9ccbfaf5cff41731191fcbb2f2cf
sjhsbhqf <1549097063@qq.com>
2024-07-03 11:23:10 +0800

    20240703112310

---
+++ quick-add-commit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 获取当前的日期和时间
+current_datetime=$(date +"%Y%m%d%H%M%S")
+
+# 添加所有更改到暂存区
+git add .
+
+# 提交更改，使用当前日期和时间作为提交消息
+git commit -m "$current_datetime"
+
+# 推送更改到远程仓库
+git push
